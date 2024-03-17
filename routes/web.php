5d2ca1be3d782f7e9b0432f6fb4acf1267051783
Amgad Wageh <you@example.com>
2024-03-17 10:36:23 +0200

    composer edit and web route

--- routes/web.php
+++ routes/web.php
@@ -1,56 +1,75 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-/*
+
+$router->get('/', function ()  use ($router)
+{
+    return $router->app->version();
+});
+
 $router->get('/test', function () {
-    print  ('hello from route ');
+    return 'Test';
 });
 //mandatory param
-$router->get('user/{id}', function ($id) {
+$router->get('moderator/{id}', function ($id) {
     return 'User '.$id;
 });
-//multi parameters
+/////optinal parameters
+$router->get('admin[/{name}]', function ($name = null) {
+    return $name;
+});
+//multi mandatory params parameters
 $router->get('posts/{postId}/comments/{commentId}', function ($postId, $commentId) {
     return 'post num '.$postId;
 });
-/////optinal parameters
-$router->get('user[/{name}]', function ($name = null) {
+//Regular expression force hime to enter name in characters
+$router->get('asd/{name:[A-Za-z]+}', function ($name) {
     return $name;
+
 });
-//named routes///////////////////////////////////////////
-$router->get('profile', ['as' => 'profile', function () {
+//named routes//
+$router->get('dataa', ['as' => 'profile', function () {
     return route('profile');
 }]);
-//route prefix//route prefix//route prefix//route prefix//route prefix
+//route prefix
 $router->group(['prefix' => 'user'], function () use ($router) {
     $router->get('getAll', function () {
-        // Matches The "/user/getAll" URL
         return 'getAllUsers' ;
     });
     $router->post('adddUSer', function () {
-        // Matches The "/user/getAll" URL
         return 'adddUSer' ;
+    });
+});
+//MiddleWare
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->get('/', function () {
+        // Uses Auth Middleware
+    });
+
+    $router->get('senior/profile', function () {
+        // Uses Auth Middleware
+    });
+});
+///////
+//Namespaces
+$router->group(['namespace' => 'Admin'], function() use ($router)
+{
+    // Using The "App\Http\Controllers\Admin" Namespace...
+    $router->group(['namespace' => 'User'], function() use ($router) {
+        // Using The "App\Http\Controllers\Admin\User" Namespace...
     });
 });
 //using middleWare
 $router->get('age' ,['middleware' => 'age:10', function () {
     print  ('hello from route age  ');
 }]);
-*//*
-$router->group(['middleware' => ['token','xapikey','ipWhitelist'], function () use ($router) {
-    $router->get('/', function () {
-        return $router->app->version();
-    });
 
-    $router->get('user/profile', function () {
-        // Uses Auth Middleware
-    });
-});*/
-
-$router->get('/', function () {
-    print  ('hello from route ');
+$router->get('user/profile', function () {
+    return $router->app->version();
 });
+
+
+
 $router->post('/login', [
     'middleware' => [ 'xapikey' ,'ipWhitelist'],
     'uses' => 'AdminController@login'
@@ -63,3 +82,15 @@ $router->post('/banIP', [
     'middleware' => [ 'xapikey' ,'ipWhitelist'],
     'uses' => 'AdminController@banIP'
 ]);
+
+/*$router->group(['middleware' => ['token','xapikey','ipWhitelist'], function () use ($router) {
+    $router->get('/', function () {
+        return $router->app->version();
+    });*/
+    $router->get('/userpost/{id}', ['middleware' => 'auth', function (Request $request, $id)
+    {
+        $user = Auth::user();
+        $user = $request->user();
+    }]);
+    $router->get('showadmin/{id}', 'AdminController@show');
+//HTTP Requests
